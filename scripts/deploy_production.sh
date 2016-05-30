7ca8f5b5633c2b082465afdf4f43bebfcf556fa2
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-30 23:54:08 +0900

    set default region

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-AWS_DEFAULT_REGION="ap-northeast-1"
+export AWS_DEFAULT_REGION="ap-northeast-1"
 SECURITY_GROUP_NAME="circleci"
 IP=`curl inet-ip.info`
 
-trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
+trap "aws ec2 revoke-security-group-ingress --region ${AWS_DEFAULT_REGION} --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --region ${AWS_DEFAULT_REGION} --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
 
 scp ~/.aws/config deploy@52.69.196.119:~/
 
