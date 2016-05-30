189b5362e720d8eba650700138e62401a729d9f3
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-30 22:40:08 +0900

    EC2 ssh test again

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_DEFAULT_REGION="ap-northeast-1"
-
 SECURITY_GROUP_NAME="circleci"
 IP=`curl -s ifconfig.me`
 
