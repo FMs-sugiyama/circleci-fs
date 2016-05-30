4bc0c12f74a0f674e37976d1ebab16cd1cce4d97
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-30 23:31:06 +0900

    ec2 scp test

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -8,6 +8,6 @@ SECURITY_GROUP_NAME="circleci"
 # trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
 # aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
 
-scp ~/.aws/config 52.69.196.119:/tmp/
+scp ~/.aws/config deploy@52.69.196.119:~/
 
 echo 'production'
