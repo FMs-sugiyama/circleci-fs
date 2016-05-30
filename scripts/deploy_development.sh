b39c14a94adf19e8fec228a842fbff54e5a4530b
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-31 02:47:48 +0900

    production deploy

--- scripts/deploy_development.sh
+++ scripts/deploy_development.sh
@@ -2,16 +2,14 @@
 
 set -ex
 
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-northeast-1"
-
 SECURITY_GROUP_NAME="circleci"
-IP=`curl -s ifconfig.me`
+IP=`curl inet-ip.info`
 
-trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
+trap "aws ec2 revoke-security-group-ingress --region ${AWS_DEFAULT_REGION} --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --region ${AWS_DEFAULT_REGION} --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
 
-cat ~/.aws/config
-cat ~/.aws/credentials
+scp circleci-web/target/circleci-web-1.0.war deploy@52.69.196.119:~/circleci-web.war
+ssh deploy@52.69.196.119 'sh ~/deploy.sh'
 
+echo 'end development release'
