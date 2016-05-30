eabb54d866978691e2e1c4debb20035abbe91969
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-30 22:37:28 +0900

    EC2 ssh test again

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+set -ex
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+SECURITY_GROUP_NAME="circleci"
+IP=`curl -s ifconfig.me`
+
+trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
+
+cat ~/.aws/config
+cat ~/.aws/credentials
+
 echo 'production'
