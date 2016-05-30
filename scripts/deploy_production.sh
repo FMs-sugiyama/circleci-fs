50b363da360b61f966170767650798798294f8d7
杉山毅 <sugiyama@fullmarks.co.jp>
2016-05-30 23:43:50 +0900

    change ifconfig.me(too slow)

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -3,7 +3,7 @@
 set -ex
 
 SECURITY_GROUP_NAME="circleci"
-IP=`curl -s ifconfig.me`
+IP=`curl inet-ip.info`
 
 trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
